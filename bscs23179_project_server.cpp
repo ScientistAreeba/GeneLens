50619dddb60beea81b63444f20f16be8f50f2085
AreebaAtif <areebaaatif6@gmail.com>
2025-04-20 17:13:22 +0500

    added mutation types to detect chnages genetic alignment
     nucleotide sequences were taken from client to detect changes in genetic alignment to specify is the DNA sequence : added, deleted or replaced

--- bscs23179_project_server.cpp
+++ bscs23179_project_server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "sqlite3.h"
 #define NOMINMAX
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -16,30 +17,33 @@ string healthyG = "ATGCGTAGTC";
 struct AlignmentScore
 {
     int score;
-
+    string aligned_input;
+    string aligned_healthy;
+    int updations;
+    int insertions;
+    int deletions;
 };
 
 
-AlignmentScore smithWaterman( const string& input, const string& healthy, int match = 2,int mismatch = -1, int gap = -2 )
+AlignmentScore smithWaterman(const string& input, const string& healthy, int match = 2, int mismatch = -1, int gap = -2)
 {
     int o = input.size();
     int h = healthy.size();
-
     vector<vector<int>> dp(o + 1, vector<int>(h + 1, 0));
-    vector<vector<int>> traceback(o + 1, vector<int>(h + 1, 0));
+    vector<vector<int>> tracing(o + 1, vector<int>(h + 1, 0));
 
+    int maxScore = 0;
     int max1 = 0;
     int max2 = 0;
-    int maxScore = 0;
-    
 
-    for (int i = 1; i <= o; ++i) 
+
+    for (int i = 1; i <= o; ++i)
     {
-        for (int j = 1; j <= h; ++j) 
+        for (int j = 1; j <= h; ++j)
         {
-            
+
             int diagonalScore;
-            if (input[i - 1] == healthy[j - 1]) 
+            if (input[i - 1] == healthy[j - 1])
             {
                 diagonalScore = dp[i - 1][j - 1] + match;
             }
@@ -47,26 +51,26 @@ AlignmentScore smithWaterman( const string& input, const string& healthy, int ma
                 diagonalScore = dp[i - 1][j - 1] + mismatch;
             }
 
-          
+
             int UpScore = dp[i - 1][j] + gap;
             int LeftScore = dp[i][j - 1] + gap;
-            
+
             dp[i][j] = max({ 0, diagonalScore, UpScore, LeftScore });
 
             if (dp[i][j] == diagonalScore)
             {
-                traceback[i][j] = 1;
+                tracing[i][j] = 1;
             }
             else if (dp[i][j] == UpScore)
             {
-                traceback[i][j] = 2;
+                tracing[i][j] = 2;
             }
             else if (dp[i][j] == LeftScore)
             {
-                traceback[i][j] = 3;
+                tracing[i][j] = 3;
             }
 
-            if (dp[i][j] > maxScore) 
+            if (dp[i][j] > maxScore)
             {
                 maxScore = dp[i][j];
                 max1 = i;
@@ -74,18 +78,59 @@ AlignmentScore smithWaterman( const string& input, const string& healthy, int ma
             }
         }
     }
-    
-  
 
-    return { maxScore };
+
+    string alignInput;
+    string alignHealthy;
+    int i = max1;
+    int j = max2;
+
+    int update = 0;
+    int insert = 0;
+    int remove = 0;
+
+
+    while (i > 0 && j > 0 && dp[i][j] != 0)
+    {
+        if (tracing[i][j] == 1)
+        {
+            alignInput = input[i - 1] + alignInput;
+            alignHealthy = healthy[j - 1] + alignHealthy;
+            if (input[i - 1] != healthy[j - 1]) ++update;
+            --i; --j;
+        }
+        else if (tracing[i][j] == 2)
+        {
+            alignInput = input[i - 1] + alignInput;
+            alignHealthy = "-" + alignHealthy;
+            ++insert;
+            --i;
+        }
+        else if (tracing[i][j] == 3)
+        {
+            alignInput = "-" + alignInput;
+            alignHealthy = healthy[j - 1] + alignHealthy;
+            ++remove;
+            --j;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return { maxScore, alignInput, alignHealthy, update, insert, remove };
+
 }
 
-void clientComputation(SOCKET clientSocket) 
+
+
+void clientComputation(SOCKET clientSocket)
 {
     char bufferSpace[bufferCapacity] = { 0 };
     int inputSize = recv(clientSocket, bufferSpace, bufferCapacity - 1, 0);
 
-    if (inputSize <= 0) 
+    if (inputSize <= 0)
     {
         cerr << "Failed to receive gene sequence.\n";
         closesocket(clientSocket);
@@ -94,15 +139,10 @@ void clientComputation(SOCKET clientSocket)
 
     bufferSpace[inputSize] = '\0';
     string clientG(bufferSpace);
+
     cout << "\n Received DNA from client: " << clientG << "\n";
 
-    
-    AlignmentScore score = smithWaterman(clientG, healthyG);
-
- 
-    string output = "Alignment Score: " + std::to_string(score.score) + "\n";
-    send(clientSocket, output.c_str(), output.size(), 0);
-    cout << "Alignment score send to client: " << score.score << endl;
+    //compareWithHealthyGenesFromDB("GenesDatabase.db", clientG, clientSocket);
 
     closesocket(clientSocket);
 }
@@ -112,7 +152,7 @@ void listenPorThread(int port)
     SOCKET ss = socket(AF_INET, SOCK_STREAM, 0);
     if (ss == INVALID_SOCKET)
     {
-        cerr << " Socket couldnot built."<<endl;
+        cerr << " Socket couldnot built." << endl;
         return;
     }
 
@@ -123,7 +163,7 @@ void listenPorThread(int port)
 
     if (bind(ss, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
     {
-        cerr << " Bind failed."<< endl;
+        cerr << " Bind failed." << endl;
         closesocket(ss);
         return;
     }
@@ -135,7 +175,7 @@ void listenPorThread(int port)
         return;
     }
 
-    cout << " Server is listening on port " << port << endl;
+    cout << " Server is listening on port \n" << port << endl;
 
     while (true)
     {
@@ -143,7 +183,7 @@ void listenPorThread(int port)
         int client_size = sizeof(client);
         SOCKET client_socket = accept(ss, (sockaddr*)&client, &client_size);
 
-        if (client_socket != INVALID_SOCKET) 
+        if (client_socket != INVALID_SOCKET)
         {
             thread client_thread(clientComputation, client_socket);
             client_thread.detach();
@@ -152,7 +192,6 @@ void listenPorThread(int port)
 
     closesocket(ss);
 }
-
 
 
 int main()
