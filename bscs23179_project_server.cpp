2aa0704cabde1d8a450b86732d2867b570bf7084
AreebaAtif <areebaaatif6@gmail.com>
2025-04-27 13:27:45 +0500

    server is the load balancer now

--- bscs23179_project_server.cpp
+++ bscs23179_project_server.cpp
@@ -1,485 +1,108 @@
 #include <iostream>
-#include "sqlite3.h"
-#define NOMINMAX
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>
-#include <string>
 #include <vector>
 using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 #define bufferCapacity 1024
 
-
-
-struct AlignmentScore
+void forwardToWorker(const string& inputGene, SOCKET clientSocket, int preferredWorkerPort)
 {
-    int score;
-    string aligned_input;
-    string aligned_healthy;
-    int substitution;
-    int insertions;
-    int deletions;
-};
+    int fallbackPort = 8000;
 
-AlignmentScore smithWaterman(const string& input, const string& healthy, int match = 2, int mismatch = -1, int gap = -2)
-{
-    int o = input.size();
-    int h = healthy.size();
-    vector<vector<int>> dp(o + 1, vector<int>(h + 1, 0));
-    vector<vector<int>> tracing(o + 1, vector<int>(h + 1, 0));
+    // Try preferred port first
+    SOCKET workerSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in workerAddr{};
+    workerAddr.sin_family = AF_INET;
+    workerAddr.sin_port = htons(preferredWorkerPort);
+    inet_pton(AF_INET, "127.0.0.1", &workerAddr.sin_addr);
 
-    int maxScore = 0;
-    int max1 = 0;
-    int max2 = 0;
-
-
-    for (int i = 1; i <= o; ++i)
+    if (connect(workerSocket, (sockaddr*)&workerAddr, sizeof(workerAddr)) == SOCKET_ERROR)
     {
-        for (int j = 1; j <= h; ++j)
+        cerr << "Server: Worker on port " << preferredWorkerPort << " not available. Trying fallback port " << fallbackPort << "...\n";
+        closesocket(workerSocket);
+
+        // Try fallback
+        workerSocket = socket(AF_INET, SOCK_STREAM, 0);
+        workerAddr.sin_port = htons(fallbackPort);
+
+        if (connect(workerSocket, (sockaddr*)&workerAddr, sizeof(workerAddr)) == SOCKET_ERROR)
         {
-
-            int diagonalScore;
-            if (input[i - 1] == healthy[j - 1])
-            {
-                diagonalScore = dp[i - 1][j - 1] + match;
-            }
-            else {
-                diagonalScore = dp[i - 1][j - 1] + mismatch;
-            }
-
-
-            int UpScore = dp[i - 1][j] + gap;
-            int LeftScore = dp[i][j - 1] + gap;
-
-            dp[i][j] = max({ 0, diagonalScore, UpScore, LeftScore });
-
-            if (dp[i][j] == diagonalScore)
-            {
-                tracing[i][j] = 1;
-            }
-            else if (dp[i][j] == UpScore)
-            {
-                tracing[i][j] = 2;
-            }
-            else if (dp[i][j] == LeftScore)
-            {
-                tracing[i][j] = 3;
-            }
-
-            if (dp[i][j] > maxScore)
-            {
-                maxScore = dp[i][j];
-                max1 = i;
-                max2 = j;
-            }
+            cerr << "Server: Fallback worker on port " << fallbackPort << " also not available.\n";
+            closesocket(workerSocket);
+            return;
         }
     }
 
+    send(workerSocket, inputGene.c_str(), inputGene.size(), 0);
 
-    string alignInput;
-    string alignHealthy;
-    int i = max1;
-    int j = max2;
-
-    int substitues = 0;
-    int insert = 0;
-    int deletes = 0;
-
-
-    while (i > 0 && j > 0 && dp[i][j] != 0)
+    char buffer[bufferCapacity] = { 0 };
+    int bytesReceived = recv(workerSocket, buffer, bufferCapacity - 1, 0);
+    if (bytesReceived > 0)
     {
-        if (tracing[i][j] == 1)
-        {
-            alignInput = input[i - 1] + alignInput;
-            alignHealthy = healthy[j - 1] + alignHealthy;
-            if (input[i - 1] != healthy[j - 1]) ++substitues;
-            --i; --j;
-        }
-        else if (tracing[i][j] == 2)
-        {
-            alignInput = input[i - 1] + alignInput;
-            alignHealthy = "-" + alignHealthy;
-            ++insert;
-            --i;
-        }
-        else if (tracing[i][j] == 3)
-        {
-            alignInput = "-" + alignInput;
-            alignHealthy = healthy[j - 1] + alignHealthy;
-            ++deletes;
-            --j;
-        }
-        else
-        {
-            break;
-        }
+        buffer[bytesReceived] = '\0';
+        send(clientSocket, buffer, bytesReceived, 0);
     }
 
-    return { maxScore, alignInput, alignHealthy, substitues, insert, deletes };
-
+    closesocket(workerSocket);
 }
 
-
-
-void CreateAndInsertDB(const string& dbPath)
+void handleClient(SOCKET clientSocket, int workerPort)
 {
-    sqlite3* db;
-    char* errMsg = nullptr;
-
-    int rc = sqlite3_open(dbPath.c_str(), &db);
-    if (rc)
+    char buffer[bufferCapacity] = { 0 };
+    int bytes = recv(clientSocket, buffer, bufferCapacity - 1, 0);
+    if (bytes <= 0)
     {
-        cerr << "couldnt open database: " << sqlite3_errmsg(db) << endl;
-        return;
-    }
-
-    //healthy gene database
-    const char* createHgeneTableSQL = R"(
-        CREATE TABLE IF NOT EXISTS HealthyGenes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            sequence TEXT,
-            UNIQUE(name, sequence)
-        );
-    )";
-
-    rc = sqlite3_exec(db, createHgeneTableSQL, nullptr, nullptr, &errMsg);
-
-    if (rc != SQLITE_OK)
-    {
-        cerr << "SQL error (HGenes): " << errMsg << endl;
-        sqlite3_free(errMsg);
-        sqlite3_close(db);
-        return;
-    }
-
-    const char* hGene[][2] =
-    {
-     {"BRCA1", "ATGAAAAAACTGAGTAAGGAAAGCCTGAGCCAGAGGGTGTCCCGGCTGGGGCATGTGGAGGGTGACTGT"},
-     {"TP53", "ATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGAAACATTTTCAGACCTATGGAAACTACTTCCTGAAAACAACGTTCTGT"},
-     {"CFTR", "ATGTTCGTCTTCCTGGATTATGCCTGGCACCATTAAAGAAAATATCATCTTTGGTGTTTCCTATGATGAACACTTGGTTGGC"},
-     {"HBB", "ATGGTGCACCTGACTCCTGAGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAG"},
-     {"INS", "ATGCCCTGTGGATGCGCCTCCTGCACCCCAGGCTTTTGTCAAACAGCACCTTTGTGGTTCTCACTGGTGGGCGCTCAGCCTATCTTG"},
-     {"APOE", "ATGAGGCCAGAGGGTCCAGGAGGAAGGTGAGTGAAGAGGGAGTGGAGGGAAGAGGAAGGGAAGGGAGGGAAGAGGAAGGGAGGG"},
-     {"BRCA2", "ATGGAGGAGCTCGAGTCGAGGAAGGAGGAGGAGAGGAGGAGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGG"},
-     {"EGFR", "ATGAAAGAGGAGGAGGAGGGAGGAGGAAGGAGGGAGGAGGGAGGGAGAGGGAGGGAGGAGGAGGGAGGGAGGGAGGAGGGAGGAAGG"},
-     {"FTO", "ATGGAGAGGAGAGGAAGAGGGAGAGGAAGGAGGGAGAGGAAGGGAGGGAGGAGGAGGAGGAAGGAGGAAGGGAGGGAGGGAGGGAGGAGGGAGG"},
-     {"MTOR", "ATGAGAGGAGGGAGGGAGGGAGGGAGGGAGGAGGGAGGAGGGAGGGAGGGAGGGAGGGAGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGG"},
-     {"VHL", "ATGGAGGAGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGGAGGG"},
-     {"G6PD", "ATGCCGCAGGGCCTCTCAGGCCAGGGCCTCCTGCGGCTCTGGCCTCTCGGCAGCGTGCAGGCCTCTCTGCCAGGGGCGTCTC"},
-     {"DMD", "ATGGATGTCATTTGGGAAAGGAGAGGTGGATGAAGTGAAGGAGGAGGAGGAAGAGGAAGGAGGGAGAGGAGGAGGGGAGGAGGA"},
-     {"MYH7", "ATGCCTCGTGCGGCGGTCTCCTGCTGCCTCCTGCCTGCTGCTCCTGCTGCCTGCTCCTGCTGCTGCTGCTGCTGCAGCTG"},
-     {"LDLR", "ATGGAGACAGCAGCAGGCGGGGAGGCGGCGCAGCGGGAGGGCGAGGCGCAGCGGCGGAGCGGCGCAGCGGGAGCGGC"},
-     {"P53BP1", "ATGGAGGTAGGAGGAAGGAGGAAGGAAGGAAGGAAGGAGGAGGAGGAAGGAGGAAGGAAGGAGGAAGGAGGAGG"},
-     {"TTN", "ATGGCCGGAGGTGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAGGAG"},
-     {"COL1A1", "ATGGCCTCCCTGGTGGAGCTGGAGGGGCTGGAGGTGGAGGAGGCTGGAGGAGGAGGCTGGAGGAGGAGGAG"},
-     {"ACTN3", "ATGGCCGAGCGGCGGAGCGGCGGCGGAGCGGCGGAGCGGCGGCGGAGCGGCGGCGGAGCGGCGGCGGAGC"},
-     {"VEGFA", "ATGAACTTTCTGCTGTCTTGGGTGCATTGGAGCCTTGCCTTGCTGCTCTACCTCCACCATGCCAAGTGGTCCCAGGCTGC"}
-    };
-
-
-
-    const char* insertHgeneSQL = R"(
-    INSERT OR IGNORE INTO HealthyGenes (name, sequence)
-    VALUES (?, ?);
-    )";
-
-    sqlite3_stmt* insertHgeneStmt;
-
-    rc = sqlite3_prepare_v2(db, insertHgeneSQL, -1, &insertHgeneStmt, nullptr);
-    if (rc != SQLITE_OK)
-    {
-        cerr << "preparation failed at Hgene: " << sqlite3_errmsg(db) << "\n";
-        sqlite3_close(db);
-        return;
-    }
-
-    for (int i = 0; i < sizeof(hGene) / sizeof(hGene[0]); ++i)
-    {
-        sqlite3_reset(insertHgeneStmt);
-        sqlite3_clear_bindings(insertHgeneStmt);
-        sqlite3_bind_text(insertHgeneStmt, 1, hGene[i][0], -1, SQLITE_STATIC);
-        sqlite3_bind_text(insertHgeneStmt, 2, hGene[i][1], -1, SQLITE_STATIC);
-
-        rc = sqlite3_step(insertHgeneStmt);
-        if (rc != SQLITE_DONE)
-            cerr << "Insertion failed at Hgene: " << sqlite3_errmsg(db) << endl;
-    }
-
-    sqlite3_finalize(insertHgeneStmt);
-
-
-
-    // mutation gene database
-    const char* createMgeneTableSQL = R"(
-        CREATE TABLE IF NOT EXISTS MutationGenes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            geneName TEXT,
-            mutationSequence TEXT,
-            disease TEXT,
-            UNIQUE(geneName, mutationSequence)
-        );
-    )";
-
-    rc = sqlite3_exec(db, createMgeneTableSQL, nullptr, nullptr, &errMsg);
-    if (rc != SQLITE_OK)
-    {
-        cerr << "SQL error (MGenes): " << errMsg << endl;
-        sqlite3_free(errMsg);
-        sqlite3_close(db);
-        return;
-    }
-
-
-    const char* insertMgeneSQL = R"(
-        INSERT OR IGNORE INTO MutationGenes (geneName, mutationSequence, disease)
-        VALUES (?, ?, ?);
-    )";
-
-    const char* mGene[][3] =
-    {
-        {"BRCA1", "ACTGAGTAAGGAAAGCCTGAGCCAGAGGGTGTGTGTGTCCAGTTTCTGTTCTTGCAG", "Breast Cancer"},
-        {"TP53", "AGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGCCCTTGTCCAGCTCTGGGGAGAGG", "Li-Fraumeni Syndrome"},
-        {"CFTR", "ATGTTCGTCTTCCTGGATTATGCCTGGCACCTGCCGTTTTGATGACGCTTCACTG", "Cystic Fibrosis"},
-        {"HBB", "CCTGACTCCTGAGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTG", "Sickle Cell Anemia"},
-        {"HTT", "CAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGCAG", "Huntington's Disease"},
-        {"FBN1", "TGAGGACTTCAGTGAGAGGAGACTTCCAGAGTTGGCTTCCAGAGTCTTCAGTGCAG", "Marfan Syndrome"},
-        {"MLH1", "GTGTAGTAGGAGGAGGTTGGAAGTGGATGAGGAGGCTGTTGTAGATGAGTAGGAG", "Hereditary Nonpolyposis Colorectal Cancer"},
-        {"PAH", "GGAGTGGGAGTGGTGGTGTGGTGAGGTGGAGTGAGGCTGTGGGAGTGGCTGTGG", "Phenylketonuria"},
-        {"DMD", "TGGTCTGGAGGTGTGGTGTGGAGGAGGTGGTGGAGGTGGTGGTGGTGGTGGAGG", "Duchenne Muscular Dystrophy"},
-        {"SMN1", "GCTGAGGGTGTGTGTGGTGGAGGAGGTGGGTGGAGGAGGTGTGGAGGTGAGGTG", "Spinal Muscular Atrophy"},
-        {"GBA", "GGAGGTGGAGGAGGAGGTGGGTGTGGGTGGAGGTGTGGAGGAGGTGTGGTGGTG", "Gaucher's Disease"},
-        {"NPC1", "CTGCTGAGGAGGGTGGAGGAGGTGGAGGGTGGAGGTGGTGGTGGAGGTGGTGGA", "Niemann-Pick Disease Type C"},
-        {"MECP2", "AGCTGAGGAGGTGGAGGAGGTGGTGGAGGAGGTGGGTGGAGGAGGTGGAGGTGG", "Rett Syndrome"},
-        {"TSC1", "GTGGGTGGAGGTGGAGGTGGAGGAGGTGGAGGTGGTGGAGGTGGGTGGTGGGTG", "Tuberous Sclerosis"},
-        {"PKD1", "GGAGGTGGAGGAGGTGGTGGAGGAGGTGGTGGGTGGAGGTGGAGGGTGGAGGTG", "Polycystic Kidney Disease"}
-    };
-
-
-    sqlite3_stmt* insertMgeneStmt;
-    rc = sqlite3_prepare_v2(db, insertMgeneSQL, -1, &insertMgeneStmt, nullptr);
-    if (rc != SQLITE_OK)
-    {
-        cerr << "preparation failed Mgene: " << sqlite3_errmsg(db) << endl;
-        sqlite3_close(db);
-        return;
-    }
-
-    for (int i = 0; i < sizeof(mGene) / sizeof(mGene[0]); ++i)
-    {
-        sqlite3_reset(insertMgeneStmt);
-        sqlite3_clear_bindings(insertMgeneStmt);
-        sqlite3_bind_text(insertMgeneStmt, 1, mGene[i][0], -1, SQLITE_STATIC);
-        sqlite3_bind_text(insertMgeneStmt, 2, mGene[i][1], -1, SQLITE_STATIC);
-        sqlite3_bind_text(insertMgeneStmt, 3, mGene[i][2], -1, SQLITE_STATIC);
-
-        rc = sqlite3_step(insertMgeneStmt);
-        if (rc != SQLITE_DONE)
-            cerr << "Insertion failed at Mgene : " << sqlite3_errmsg(db) << endl;
-    }
-
-    sqlite3_finalize(insertMgeneStmt);
-    sqlite3_close(db);
-}
-
-string diseasePredictorAndMutation(const string& dbFile, const string& seq)
-{
-    
-    string Bioresult = "Disease Predictor Results:\n";
-    sqlite3* db;
-    int rc = sqlite3_open(dbFile.c_str(), &db);
-    if (rc)
-    {
-        return Bioresult + "failed to open database:" + string(sqlite3_errmsg(db)) + "\n";
-    }
-
-    const char* Diseasequery = R"(
-        SELECT geneName, disease FROM MutationGenes
-        WHERE INSTR(?, mutationSequence) > 0;
-    )";
-
-    sqlite3_stmt* stmt;
-    rc = sqlite3_prepare_v2(db, Diseasequery, -1, &stmt, nullptr);
-    if (rc != SQLITE_OK)
-    {
-        return Bioresult + "Predictor couldnt find results: " + string(sqlite3_errmsg(db)) + "\n";
-    }
-
-    sqlite3_bind_text(stmt, 1, seq.c_str(), -1, SQLITE_STATIC);
-
-    bool found = false;
-
-    while (sqlite3_step(stmt) == SQLITE_ROW)
-    {
-        const char* gene = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-        const char* disease = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
-        Bioresult += "Mutation found in gene " + string(gene) + " Disease: " + string(disease) + "\n";
-        found = true;
-    }
-
-    if (!found)
-    {
-        Bioresult += "No mutations found in the client's input sequence.\n";
-    }
-
-    sqlite3_finalize(stmt);
-    sqlite3_close(db);
-
-    return Bioresult;
-}
-
-void comparisonOfGeneSequence(const string& dbFile, const string& inputGene, SOCKET clientSocket)
-{
-    sqlite3* db;
-    sqlite3_stmt* stmt;
-    const char* selectSQL = "SELECT name, sequence FROM HealthyGenes";
-
-    if (sqlite3_open(dbFile.c_str(), &db) != SQLITE_OK)
-    {
-        cerr << "failed to open database.\n";
-        return;
-    }
-
-    if (sqlite3_prepare_v2(db, selectSQL, -1, &stmt, nullptr) != SQLITE_OK)
-    {
-        cerr << "failed to select the statement.\n";
-        sqlite3_close(db);
-        return;
-    }
-
-    int bestScore = -1;
-    string bestGene;
-    AlignmentScore bestAligned;
-
-    while (sqlite3_step(stmt) == SQLITE_ROW)
-    {
-        string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-        string Gseq = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
-
-        AlignmentScore score = smithWaterman(inputGene, Gseq);
-
-        if (score.score > bestScore)
-        {
-            bestScore = score.score;
-            bestGene = name;
-            bestAligned = score;
-        }
-    }
-
-    sqlite3_finalize(stmt);
-    sqlite3_close(db);
-
-    string diseaseResults = diseasePredictorAndMutation(dbFile, inputGene);
-
-   
-
-    string output = "Score: " + to_string(bestAligned.score) + "\n"
-        + "Aligned Input: " + bestAligned.aligned_input + "\n"
-        + "Aligned Gene: " + bestAligned.aligned_healthy + "\n"
-        + "Substitution: " + to_string(bestAligned.substitution) + "\n"
-        + "Insertions: " + to_string(bestAligned.insertions) + "\n"
-        + "Deletions: " + to_string(bestAligned.deletions) + "\n\n"
-        + diseaseResults;
-
-    send(clientSocket, output.c_str(), output.size(), 0);
-    cout << "BioInformatics results sent to client.\n";
-}
-
-
-
-void clientComputation(SOCKET clientSocket)
-{
-    char bufferSpace[bufferCapacity] = { 0 };
-    int inputSize = recv(clientSocket, bufferSpace, bufferCapacity - 1, 0);
-
-    if (inputSize <= 0)
-    {
-        cerr << "Failed to receive gene sequence.\n";
+        cerr << "Server: Failed to receive from client.\n";
         closesocket(clientSocket);
         return;
     }
 
-    bufferSpace[inputSize] = '\0';
-    string clientG(bufferSpace);
+    buffer[bytes] = '\0';
+    string inputGene(buffer);
+    cout << "Server: Received DNA from client: " << inputGene << endl;
 
-    cout << "\nReceived DNA from client: " << clientG << "\n";
-
-    comparisonOfGeneSequence("GenesDatabase.db", clientG, clientSocket);
+    forwardToWorker(inputGene, clientSocket, workerPort);
 
     closesocket(clientSocket);
 }
 
-void listenPorThread(int port)
+void listenPortThread(int listenPort, int workerPort)
 {
-    SOCKET ss = socket(AF_INET, SOCK_STREAM, 0);
-    if (ss == INVALID_SOCKET)
-    {
-        cerr << " Socket couldnot built." << endl;
-        return;
-    }
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(listenPort);
 
-    sockaddr_in server{};
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(port);
+    bind(serverSocket, (sockaddr*)&addr, sizeof(addr));
+    listen(serverSocket, 5);
 
-    if (bind(ss, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
-    {
-        cerr << " Bind failed." << endl;
-        closesocket(ss);
-        return;
-    }
-
-    if (listen(ss, 5) == SOCKET_ERROR)
-    {
-        cerr << " Listening failed.\n";
-        closesocket(ss);
-        return;
-    }
-
-    
-    cout << "\n";
-    cout << port<<" => Port is listening to Server " <<endl;
-    cout << endl;
+    cout << "Server: Listening for clients on port " << listenPort << "...\n";
 
     while (true)
     {
-        sockaddr_in client{};
-        int client_size = sizeof(client);
-        SOCKET client_socket = accept(ss, (sockaddr*)&client, &client_size);
-
-        if (client_socket != INVALID_SOCKET)
+        SOCKET clientSock = accept(serverSocket, nullptr, nullptr);
+        if (clientSock != INVALID_SOCKET)
         {
-            thread client_thread(clientComputation, client_socket);
-            client_thread.detach();
+            thread t(handleClient, clientSock, workerPort);
+            t.detach();
         }
     }
 
-    closesocket(ss);
+    closesocket(serverSocket);
 }
-
-
 
 int main()
 {
-    
-    cout << " ===============================\n";
-    cout << "  Welcome to GeneLens Central Server\n";
-    cout << " ===============================\n";
-
-    CreateAndInsertDB("GenesDatabase.db");
-
     WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        cerr << " WSAStartup failed." << endl;
-        return 1;
-    }
+    WSAStartup(MAKEWORD(2, 2), &wsa);
 
-    thread t1(listenPorThread, 9000);
-    thread t2(listenPorThread, 9001);
+    // Thread for each client port
+    thread t1(listenPortThread, 9000, 8000);
+    thread t2(listenPortThread, 9001, 8001);
 
     t1.join();
     t2.join();
@@ -487,5 +110,3 @@ int main()
     WSACleanup();
     return 0;
 }
-
-
